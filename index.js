deb606a3ae349f0f6088a4b39770fbcebbee4447
Shivang <shivangs44@gmail.com>
2018-06-29 17:28:17 -0400

    Basic syntax highlighting working

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 // Imports
 const Prism = require('prismjs');
 const fs = require('fs');
+const Chalk = require('chalk');
 
 // Parses User input file and options, if any
 // TODO: Determine possible options and parse them into options
 const parseUserInput = function () {
-	const fileName = process.argv[2];
+	const filePath = process.argv[2];
 	return {
 		file_path: filePath,
 		options: {}
@@ -16,7 +17,7 @@ const parseUserInput = function () {
 
 // Reads the file provided and returns the code in a string
 const readFile = function (filePath) {
-	const code = fs.readFileSync(fileName, 'utf-8'); // TODO: Deal with different file encodings
+	const code = fs.readFileSync(filePath, 'utf-8'); // TODO: Deal with different file encodings
 	return code;
 }
 
@@ -26,14 +27,36 @@ const tokenize = function (code) {
 	return tokens;
 }
 
-const colorizedCode = function (code, tokens) {}
 
-const printColorizedCode = function (colorizedCode) {}
+const colorizeCode = function (code, tokens) {
+	const colorMap = {
+		'string': Chalk.magenta,
+		'keyword': Chalk.blue
+	};
+
+	let colorizedCode = '';
+
+	tokens.forEach((token) => {
+		if (token !== null && typeof token === 'object' && colorMap.hasOwnProperty(token.type)) {
+			colorizedCode += colorMap[token.type](token.content);
+		} else {
+			colorizedCode += typeof token == 'object' ? token.content : token;
+		}
+	});
+
+	return colorizedCode; 
+}
+
+
+// Prints to STDOUT the colorized code
+const printColorizedCode = function (colorizedCode) {
+	console.log(colorizedCode);
+}	
 
 
 
 const userInput = parseUserInput();
 const code = readFile(userInput.file_path);
 const tokens = tokenize(code);
-const colorizedCode = colorzeCode(code, tokens);
+const colorizedCode = colorizeCode(code, tokens);
 printColorizedCode(colorizedCode);
--- myjs.js
+++ myjs.js
@@ -1,1 +1,8 @@
-var cat = 'hat';
+// This is a comment!
+
+var myFunc = function() {
+	console.log("Hello world!");
+	var a = 3;
+	var b = "cat";
+}
+
