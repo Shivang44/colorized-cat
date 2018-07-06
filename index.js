7e33ffdb73791c82ebbfaf86a200613c800dc9ee
Shivang <shivangs44@gmail.com>
2018-07-06 12:36:55 -0400

    Working language detection based on file extension

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const Prism = require('prismjs');
 const fs = require('fs');
 const Chalk = require('chalk');
+const loadLang = require('prismjs/components/index.js');
 
 // Parses User input file and options, if any
 // TODO: Determine possible options and parse them into options
@@ -17,23 +18,53 @@ const parseUserInput = function () {
 
 // Reads the file provided and returns the code in a string
 const readFile = function (filePath) {
+	let fileExtension = filePath.match(/(?:.*\/)*(?:.*\.)(.*)/);
+
+	if (fileExtension == null) {
+		console.log("File must have an extension (e.g. 'file.js' at this time)");
+		return false;
+	}
+
+	// User extensions will vary (yml vs yaml) so this maps them to prism-recognized extensions
+	const extensionMap = {
+		'yml': 'yaml'
+	};
+
+	fileExtension = fileExtension[1].toLowerCase();
+
+	if (extensionMap.hasOwnProperty(fileExtension)) {
+		fileExtension = extensionMap[fileExtension];
+	}
+
 	const code = fs.readFileSync(filePath, 'utf-8'); // TODO: Deal with different file encodings
-	return code;
+
+	return {
+		code: code,
+		lang_extension: fileExtension 
+	}
 }
 
 // Tokenize code and determine its syntatical meaning
-const tokenize = function (code) {
-	const tokens = Prism.tokenize(code, Prism.languages.javascript);
-	return tokens;
+const tokenize = function (file) {
+	const languageMap = {
+		"yaml": "yaml",
+		"js": "javascript"
+	};
+
+	const langExtension = file.lang_extension;
+	loadLang([langExtension]);
+
+	const prismLang = Prism.languages[languageMap[langExtension]];
+	return Prism.tokenize(file.code, prismLang);
 }
 
-
-const colorizeCode = function (code, tokens) {
+const colorizeCode = function (tokens) {
 	const colorMap = {
 		'string': Chalk.magenta,
 		'keyword': Chalk.blue,
 		'operator': Chalk.yellow,
-		'comment': Chalk.italic.yellowBright
+		'comment': Chalk.italic.yellowBright,
+		'key': Chalk.yellow
 	};
 
 	let colorizedCode = '';
@@ -58,7 +89,10 @@ const printColorizedCode = function (colorizedCode) {
 
 
 const userInput = parseUserInput();
-const code = readFile(userInput.file_path);
-const tokens = tokenize(code);
-const colorizedCode = colorizeCode(code, tokens);
+const file = readFile(userInput.file_path);
+if (!file) {
+	process.exit(1);
+}
+const tokens = tokenize(file);
+const colorizedCode = colorizeCode(tokens);
 printColorizedCode(colorizedCode);
